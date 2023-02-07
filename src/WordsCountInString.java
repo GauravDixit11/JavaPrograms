afc86423b619abf56e5f4bbbc2b2ca0c7bf1107e
GauravDixit11 <gdgaurav66@gmail.com>
2023-02-07 14:11:22 +0530

    ArrayList added

--- src/WordsCountInString.java
+++ src/WordsCountInString.java
@@ -1,10 +1,2 @@
-ipse.ui.editors.wizards.UntitledTextFileWizard</tags>
-          <tags>persp.perspSC:org.eclipse.jdt.ui.JavaPerspective</tags>
-          <tags>persp.perspSC:org.eclipse.debug.ui.DebugPerspective</tags>
-          <tags>persp.perspSC:org.eclipse.wst.web.ui.webDevPerspective</tags>
-          <tags>persp.perspSC:org.eclipse.ui.resourcePerspective</tags>
-          <tags>persp.viewSC:org.eclipse.ui.navigator.ProjectExplorer</tags>
-          <tags>persp.showIn:org.eclipse.ui.navigator.ProjectExplorer</tags>
-          <tags>persp.showIn:org.eclipse.wst.jsdt.ui.PackageExplorer</tags>
-          <tags>persp.showIn:org.eclipse.team.ui.GenericHistoryView</tags>
+
   
--- src/collections/ArrayListExample.java
+++ src/collections/ArrayListExample.java
@@ -1,24 +1,78 @@
-ID8ChwJ4A"/>
-                <children xsi:type="advanced:Placeholder" xmi:id="_TSKuWaYBEe2fJID8ChwJ4A" elementId="org.eclipse.ui.views.BookmarkView" toBeRendered="false" ref="_TSLVhaYBEe2fJID8ChwJ4A"/>
-                <children xsi:type="advanced:Placeholder" xmi:id="_TSKuWqYBEe2fJID8ChwJ4A" elementId="org.eclipse.ui.views.ProgressView" toBeRendered="false" ref="_TSLVhqYBEe2fJID8ChwJ4A"/>
-                <children xsi:type="advanced:Placeholder" xmi:id="_TSKuW6YBEe2fJID8ChwJ4A" elementId="org.eclipse.ui.views.TaskList" toBeRendered="false" ref="_TSLVeKYBEe2fJID8ChwJ4A"/>
-                <children xsi:type="advanced:Placeholder" xmi:id="_TSKuXKYBEe2fJID8ChwJ4A" elementId="org.eclipse.ui.views.PropertySheet" toBeRendered="false" ref="_TSLVb6YBEe2fJID8ChwJ4A"/>
-              </children>
-            </children>
-          </children>
-        </children>
-      </children>
-      <children xsi:type="basic:PartStack" xmi:id="_TSKuXaYBEe2fJID8ChwJ4A" elementId="stickyFolderRight" toBeRendered="false" containerData="2500">
-        <tags>empty</tags>
-        <children xsi:type="advanced:Placeholder" xmi:id="_TSKuXqYBEe2fJID8ChwJ4A" elementId="org.eclipse.help.ui.HelpView" toBeRendered="false" ref="_TSKvo6YBEe2fJID8ChwJ4A"/>
-        <children xsi:type="advanced:Placeholder" xmi:id="_TSKuX6YBEe2fJID8ChwJ4A" elementId="org.eclipse.ui.internal.introview" toBeRendered="false" ref="_TSLVYKYBEe2fJID8ChwJ4A"/>
-        <children xsi:type="advanced:Placeholder" xmi:id="_TSKuYKYBEe2fJID8ChwJ4A" elementId="org.eclipse.ui.cheatsheets.views.CheatSheetView" toBeRendered="false" ref="_TSLVY6YBEe2fJID8ChwJ4A"/>
-      </children>
-    </children>
-    <sharedElements xsi:type="basic:Part" xmi:id="_TSKvo6YBEe2fJID8ChwJ4A" elementId="org.eclipse.help.ui.HelpView" contributionURI="bundleclass://org.eclipse.ui.workbench/org.eclipse.ui.internal.e4.compatibility.CompatibilityView" label="Help" iconURI="platform:/plugin/org.eclipse.help.ui/icons/view16/help_view.gif" tooltip="" closeable="true">
-      <persistedState key="originalCompatibilityViewClass" value="org.eclipse.help.ui.internal.views.HelpView"/>
-      <persistedState key="originalCompatibilityViewBundle" value="org.eclipse.help.ui"/>
-      <tags>View</tags>
-      <tags>categoryTag:Help</tags>
-    </sharedElements>
-    <sharedElements xsi:type="basic:Part" xmi:id="_TSLVYKYBEe2fJID8ChwJ4A" elementId="org.eclipse
+package collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayListExample {
+
+	// ArrayList - used to dynamically handle size for array
+	// Internal Array size - 10
+	public static void main(String[] args) {
+
+		ArrayList<String> studentsName = new ArrayList<>();
+		studentsName.add("Rakesh"); // add will add element end of the list
+
+		List<Integer> list = new ArrayList<>();
+		list.add(3);
+		list.add(4);
+		list.add(7);
+		System.out.println(list);
+
+		list.add(9);
+		System.out.println(list);
+
+		// add value at specific index
+		list.add(2, 12);
+		System.out.println(list);
+
+		List<Integer> newList = new ArrayList<>();
+		newList.add(150);
+		newList.add(160);
+
+		// Add List inside the list
+		list.addAll(newList);
+		System.out.println(list);
+
+		// Get the element
+		System.out.println(list.get(1));
+
+		// Set the value to index
+		list.set(2, 200);
+		System.out.println(list);
+
+		// check element present or not
+		System.out.println(list.contains(4));
+
+		// Remove element from the list
+		list.remove(3); // with index value
+		System.out.println(list);
+
+		// Remove element with type of element
+		list.remove(Integer.valueOf(150));
+		System.out.println(list);
+
+		// clear the list
+		//list.clear();
+		//System.out.println(list);
+		
+		//Ways to Iterate the list
+		//1. For Loop
+		for(int i=0;i<list.size();i++) {
+			System.out.println("The Element is "+list.get(i));
+		}
+		
+		//2. For Each
+		for(Integer element : list) {
+			System.out.println("Foreach Element is "+element);
+			
+		}
+		
+		//3. Iterator 
+		Iterator<Integer> it = list.iterator();
+		
+		while(it.hasNext()) {
+			System.out.println("Iterator "+it.next());
+		}
+	}
+}
